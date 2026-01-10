933dbd83090c428dc98b42306aab6fcd5951aa04
SdO <sdornano@gmail.com>
2026-01-10 19:24:59 +0100

    Add files via upload

---
+++ ChartModal1/ScriptChildModalFile.sql
@@ -0,0 +1,12 @@
+SELECT 'html' AS component;
+SELECT  
+'<script>
+// Après sauvegarde réussie ou bouton Close
+function notifyParentAndClose() {
+  window.parent.postMessage({ action: 'refresh', source: 'workpackage_edit' }, '*');
+  
+  // Émettre aussi l'événement SQLPage pour compatibilité
+  window.dispatchEvent(new CustomEvent("sqlpage:modal:close"));
+}
+</script>''
+ AS html;
